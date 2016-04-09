d82564c0ca6ec0ea8917fb2dd4b43c0125ce2d3e
chrisn <chrisn>
2016-04-09 15:18:44 -0400

    added kefir

--- webpack-bare-bp/myapp/src/entry.js
+++ webpack-bare-bp/myapp/src/entry.js
@@ -1,11 +1,40 @@
 import React, {Component}from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory } from 'react-router';
+import Kefir from 'kefir';
 
 export default class App extends Component {
+
+	constructor() {
+		super()
+		this.state = {
+			text: "My Text"
+		}
+		
+	}
+
+	componentDidMount() {
+
+		var cnt = 0;
+		console.log('mounted');
+		
+		this.setState({
+			text: "mounted"
+		});
+
+		var btnClicks = Kefir.fromEvents(document.querySelector('#myBtn1'), 'click');
+
+		var clicksCount = btnClicks.scan(sum => sum + 1, 0);
+
+		clicksCount.onValue( value => this.setState({text: value}))
+	}
+
   render() {
     return (
-      <h1>Hello, world!!!</h1>
+    	<div>
+		      <h1>{this.state.text}</h1>
+		      <button id="myBtn1">click me</button>
+      	</div>
     );
   }
 }
